cdae7c980bf543095c4e0f69863aeb7db0aba31f
Michael Whitham <michaelawhitham@hotmail.co.uk>
2022-10-31 16:14:08 +0000

    add a home page with categories, allow reviews of said categories to be viewed in seperate pages with unique urls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Categories from "./Components/Categories";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReviewPageBody from "./Components/ReviewPageBody";
+import { useState } from "react";
 
 const App = () => {
+  const [reviewCategory, setReviewCategory] = useState("");
+
   return (
     <div>
       <Header />
-      <Categories />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <Categories
+                setReviewCategory={setReviewCategory}
+                reviewCategory={reviewCategory}
+              />
+            }
+          />
+          <Route
+            path={`/${reviewCategory}/reviews`}
+            element={<ReviewPageBody reviewCategory={reviewCategory} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- src/Components/Sorting.jsx
+++
@@ -1,9 +0,0 @@
-// const Sorting = () => {
-//   return (
-//     <div id="sorting">
-//       <h2>Sort</h2>
-//     </div>
-//   );
-// };
-
-// export default Sorting;
