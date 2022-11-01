abcc1a0fa49277968c28c852766d6267d220e5e2
Michael Whitham <michaelawhitham@hotmail.co.uk>
2022-11-01 14:53:05 +0000

    error handling

--- src/Components/ReviewPageBody.jsx
+++ src/Components/ReviewPageBody.jsx
@@ -23,27 +23,36 @@ const ReviewPageBody = ({ reviewCategory, setReviewID }) => {
 
   return (
     <div>
-      <div>
-        <h2>{reviewCategory} - reviews</h2>
-      </div>
-      <div id="reviewpage">
-        {reviews.map((review) => {
-          if (reviewCategory === review.category)
-            return (
-              <Card id="reviewCard" key={review.review_id}>
-                <Card.Title>{review.title}</Card.Title>
-                <Card.Text>Category: {review.category}</Card.Text>
-                <Card.Text>{review.review_body}</Card.Text>
-                <Card.Text>Owner: {review.owner}</Card.Text>
-                <Link to={`/reviews/${review.review_id}`}>
-                  <Button onClick={() => setReviewID(review.review_id)}>
-                    Review
-                  </Button>
-                </Link>
-              </Card>
-            );
-        })}
-      </div>
+      {error ? (
+        <div id="error">
+          <h2>Error</h2>
+          <p>{error}</p>
+        </div>
+      ) : (
+        <div>
+          <div>
+            <h2>{reviewCategory} - reviews</h2>
+          </div>
+          <div id="reviewpage">
+            {reviews.map((review) => {
+              if (reviewCategory === review.category)
+                return (
+                  <Card id="reviewCard" key={review.review_id}>
+                    <Card.Title>{review.title}</Card.Title>
+                    <Card.Text>Category: {review.category}</Card.Text>
+                    <Card.Text>{review.review_body}</Card.Text>
+                    <Card.Text>Owner: {review.owner}</Card.Text>
+                    <Link to={`/reviews/${review.review_id}`}>
+                      <Button onClick={() => setReviewID(review.review_id)}>
+                        Review
+                      </Button>
+                    </Link>
+                  </Card>
+                );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
