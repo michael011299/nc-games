be994b0491db6a2485acfe305906c71baa4fc828
Michael Whitham <michaelawhitham@hotmail.co.uk>
2022-11-01 14:41:24 +0000

    sorting cats

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -3,48 +3,55 @@ import Button from "react-bootstrap/Button";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { getCategories } from "../APIcalls";
 
-const Categories = ({ setReviewCategory, reviewCategory }) => {
-  const [reviews, setReviews] = useState([]);
+const Categories = ({ setReviewCategory, reviews }) => {
   const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://michaels-back-end-project-22.herokuapp.com/api/reviews")
-      .then((response) => response.json())
-      .then((data) => {
-        setReviews(data.reviews);
-      });
-  }, [reviews]);
-
-  useEffect(() => {
-    fetch("https://michaels-back-end-project-22.herokuapp.com/api/categories")
-      .then((response) => response.json())
-      .then((data) => {
-        setCategories(data.categories);
+    getCategories()
+      .then((categories) => setCategories(categories))
+      .catch((err) => {
+        if (err.response.status)
+          setError(
+            `Ooops, ${err.response.request.status}, ${err.response.request.statusText}, please try again`
+          );
       });
   }, [categories]);
 
   return (
-    <div id="mainpage">
-      <h2>Categories</h2>
-      {categories.map((category) => {
-        return (
-          <Card id="categories" key={category.slug}>
-            <Card.Title id="catTitle">{category.slug}</Card.Title>
-            <Card.Text>{category.description}</Card.Text>
-            <Link to={`/${category.slug}/reviews`}>
-              <Button
-                id="selectCatButton"
-                onClick={() => {
-                  return setReviewCategory(category.slug);
-                }}
-              >
-                ➡️
-              </Button>
-            </Link>
-          </Card>
-        );
-      })}
+    <div>
+      {error ? (
+        <div id="error">
+          <h2>Error</h2>
+          <p>{error}</p>
+        </div>
+      ) : (
+        <div>
+          <h2>Categories</h2>
+          <div id="mainpage">
+            {categories.map((category) => {
+              return (
+                <Card id="categories" key={category.slug}>
+                  <Card.Title id="catTitle">{category.slug}</Card.Title>
+                  <Card.Text>{category.description}</Card.Text>
+                  <Link to={`/${category.slug}/reviews`}>
+                    <Button
+                      id="selectCatButton"
+                      onClick={() => {
+                        return setReviewCategory(category.slug);
+                      }}
+                    >
+                      ➡️
+                    </Button>
+                  </Link>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
